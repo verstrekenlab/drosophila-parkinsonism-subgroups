a1d6f42c7b4799a6a92d14442fb1284579046df0
antortjim <ntoniohu@gmail.com>
2025-09-23 16:23:26 +0000

    Clean

--- install_dependencies.R
+++
@@ -1,6 +0,0 @@
-install_dependencies <- TRUE
-
-remotes::install_github("shaliulab/behavr@0272386cd83535278d7d15c735853c2f88dae7c0", dependencies = install_dependencies)
-remotes::install_github("shaliulab/scopr@5a28ced71f6f49258e60b596d59c236f9cbe12c2", dependencies = install_dependencies)
-remotes::install_github("shaliulab/sleepr@7775e1541c93d01c0475b34be3f70570a53983b2", dependencies = install_dependencies)
-remotes::install_github("shaliulab/ggetho@60399d9d6454a24b4b85c809ba6ae05e2b1bb28e", dependencies = FALSE)
