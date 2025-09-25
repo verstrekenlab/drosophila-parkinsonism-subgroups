aa720c0b79e55b9da280e5cea059b9e8b4cba2bd
antortjim <ntoniohu@gmail.com>
2025-09-25 13:32:21 +0000

    Dont use ggetho::ggetho

--- ethoscope_QC.R
+++ ethoscope_QC.R
@@ -6,7 +6,11 @@ ethoscope_QC <- function(dt, title, output_path){
 
   asleep <- NULL
 
-  gg <- ggetho::ggetho(dt, ggplot2::aes(group = fly_no, y = asleep)) +
+  dt_bin <- behavr::rejoin(
+    behavr::bin_apply_all(dt, y = "asleep", x_bin_length = behavr::mins(30), FUN = mean)
+  )[, .(t, asleep, fly_no)]
+
+  gg <- ggplot(data = dt_bin, mapping = aes(x = t, y = asleep, group = fly_no)) +
     ggetho::stat_pop_etho() +
     ggetho::stat_ld_annotations(height = 1, alpha = 0.3, outline = NA) +
     ggplot2::facet_grid(fly_no ~ .) +
