b18bf85c76f31e3f77f409458413ac30f2a0db5b
antortjim <ntoniohu@gmail.com>
2025-09-25 13:59:49 +0000

    Clean

--- A2_mean_fraction_sleep_day_night.R
+++
@@ -1,9 +0,0 @@
-analyse_mean_fraction_of_sleep_day_vs_night <- function(dt) {
-
-  . <- asleep <- id <- phase <- NULL
-
-  sleep_fractions <- dt[, .(sleep_fraction = mean(asleep)), by = .(phase, id)]
-  sleep_fractions$asleep <- TRUE
-  return(sleep_fractions)
-
-}
