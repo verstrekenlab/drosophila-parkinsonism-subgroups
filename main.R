07447d5dd8a2b9ac0550bde33cfa34cfc6d38df7
antortjim <ntoniohu@gmail.com>
2025-09-26 16:12:35 +0000

    Clean

--- main.R
+++ main.R
@@ -16,10 +16,6 @@ for (batch_id in BATCHES) {
     out <- c(out, list(sleep_features))
 }
 
-sleep_features <- do.call(rbind, out)
-data.table::fwrite(file = "2026_kaempf_sleep_features.csv", x = sleep_features)
-
-
 out <- list()
 for (batch_id in BATCHES) {
     sleep_features <- analyse_ID_batch(batch_id, testing= TRUE)
